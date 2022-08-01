ad220784dcbdc09309aa8619e20c1c0c23493374
Sam Keel <programming@vectors.net.au>
2022-08-01 10:11:01 +0800

    todo

--- src/app/notes/notes.module.ts
+++ src/app/notes/notes.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NotesRoutingModule } from './notes-routing.module';
-
-
+import { MainComponent } from './components/main/main.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { SharedModule } from '../shared/shared/shared.module';
 
 @NgModule({
-  declarations: [
-  ],
-  imports: [
-    CommonModule,
-    NotesRoutingModule
-  ]
+  declarations: [MainComponent, TodoComponent],
+  imports: [CommonModule, NotesRoutingModule, DragDropModule, SharedModule],
 })
-export class NotesModule { }
+export class NotesModule {}
